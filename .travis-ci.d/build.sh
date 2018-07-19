76b8b8d32ac8f6e3c427412799df870d668ee67c
Eldwan Brianne <eldwan.brianne@desy.de>
2018-07-19 23:40:27 +0200

    Added files for Travis CI

---
+++ .travis-ci.d/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ILCSOFT=/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-11
+source $ILCSOFT/init_ilcsoft.sh
+
+cd /Package
+mkdir build
+cd build
+cmake -DBUILD_DOCUMENTATION=OFF -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+make
