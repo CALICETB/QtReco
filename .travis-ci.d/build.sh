a01e615e544836b070b3db2f9e0e85294afc69bc
Eldwan Brianne <ebrianne@naf-ilc13.desy.de>
2018-07-20 13:17:56 +0200

    Changed from clicdp docker image to cern sl6 image

--- .travis-ci.d/build.sh
+++ .travis-ci.d/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ILCSOFT=/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-11
+source /cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/init_gcc48.sh
 export CC=/cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/bin/gcc
 export CXX=/cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/bin/g++
 source $ILCSOFT/init_ilcsoft.sh
@@ -9,4 +10,4 @@ cd /Package
 mkdir build
 cd build
 cmake -C $ILCSOFT/ILCSoft.cmake -DBUILD_DOCUMENTATION=OFF -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
-make
+make -j4
