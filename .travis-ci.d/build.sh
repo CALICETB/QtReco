d60c4151571a61a2c17a53c5689512ba11f2bd56
Eldwan Brianne <ebrianne@naf-ilc13.desy.de>
2018-07-20 13:31:24 +0200

    Put back clicdp cvmfs config, taking ILCSoft v2016-11-22

--- .travis-ci.d/build.sh
+++ .travis-ci.d/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-export ILCSOFT=/cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/v01-17-11
-source /cvmfs/ilc.desy.de/sw/x86_64_gcc48_sl6/init_gcc48.sh
-export CC=/cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/bin/gcc
-export CXX=/cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/bin/g++
+export ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/2016-11-22/x86_64-slc6-gcc48-opt
 source $ILCSOFT/init_ilcsoft.sh
 
 cd /Package
